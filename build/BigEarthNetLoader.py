1257e97ece0e1905a5b8fa4f0937b5ced587e343
Federico Fornari <federico.fornari@ecmwf.int>
2025-02-25 12:44:55 +0100

    Added application files

---
+++ build/BigEarthNetLoader.py
@@ -0,0 +1,241 @@
+import rasterio
+from cloudpathlib import CloudPath
+import numpy as np
+import cv2
+
+import aioboto3
+import aiofiles
+import asyncio
+from botocore.exceptions import NoCredentialsError
+import concurrent.futures
+import threading
+import os
+import logging
+
+BANDS_10M = [
+    "B04",
+    "B03",
+    "B02",
+    "B08",
+]
+
+
+BANDS_20M = [
+    "B05",
+    "B06",
+    "B07",
+    "B8A",
+    "B11",
+    "B12",
+]
+
+
+
+def read_data_local(parent,name,bNeedToResize):
+    bands_data = []
+    try:
+        ALL_BANDS = BANDS_10M + BANDS_20M
+        for band_name in ALL_BANDS:
+            band_path = f"{parent}/{name}/{name}_{band_name}.tif"
+            #logging.info('band_path '+band_path)
+            with rasterio.open(band_path,driver="GTiff",sharing=False) as band_file:
+                band_data   = band_file.read(1)  # open the tif image as a numpy array
+                # Resize depending on the resolution
+                if band_name in BANDS_20M and bNeedToResize:
+                    # Carry out a bicubic interpolation (TUB does exactly this)
+                    band_data = cv2.resize(band_data, dsize=(120, 120), interpolation=cv2.INTER_CUBIC)
+                    # We have already ignored the 60M ones, and we keep the 10M ones intact
+                #logging.info("appending")
+                bands_data.append(band_data)
+            band_file.close()
+
+        bands_data = np.stack(bands_data)
+        bands_data = normalize_bands(bands_data)
+    except Exception as e:
+        logging.info('exception in read_data_local '+str(e))
+        pass
+    return bands_data
+
+
+
+async def async_read_data_local(app,parent,name, bNeedToResize,executor) -> np.ndarray:
+    try:
+        ALL_BANDS = BANDS_10M + BANDS_20M
+        data = []
+        bands_data = []
+        tasks = []
+        loop = asyncio.get_event_loop()
+        async def fetch_band_data(band_name):
+            band_path = f"{parent}/{name}/{name}_{band_name}.tif"
+            try:
+                async with aiofiles.open(band_path, mode='rb') as fileBand:
+                    data = await fileBand.read()
+                    with rasterio.io.MemoryFile(data) as memfile:
+                        #app.logger.warning('memory '+threading.current_thread().name)
+                        #band_data = await loop.run_in_executor(executor, read_band_data, memfile, bNeedToResize, band_name,app)
+                        band_data = None
+                        return band_data
+            except NoCredentialsError:
+                app.logger.warning("No AWS credentials found")
+                return None
+            except Exception as e:
+                app.logger.warning('exception'+str(e))
+        async with asyncio.TaskGroup() as tg:
+            try:
+                for band_name in ALL_BANDS:
+                    tasks.append(tg.create_task(fetch_band_data(band_name)))
+            except Exception as e:
+                app.logger.warning('exception'+str(e))
+        for task in tasks:
+            band_data = await task
+            bands_data.append(band_data)
+
+        #bands_data = np.stack(bands_data)
+        #bands_data = normalize_bands(bands_data)
+        bands_data = None
+    except Exception as e:
+        app.logger.warning('exception'+str(e))
+    return bands_data
+
+def read_band_data(memfile, bNeedToResize, band_name,app):
+    with memfile.open(driver="GTiff", sharing=False) as band_file:
+        band_data = band_file.read(1)
+        if band_name in BANDS_20M and bNeedToResize:
+            band_data = cv2.resize(band_data, dsize=(120, 120), interpolation=cv2.INTER_CUBIC)
+        return band_data
+
+
+async def async_read_data(app,bucket_name, name, bNeedToResize,client) -> np.ndarray:
+    ALL_BANDS = BANDS_10M + BANDS_20M
+    data = []
+    bands_data = []
+    tasks = []
+    async def fetch_band_data(band_name):
+        band_path = f"{name}{name[:-1]}_{band_name}.tif"
+        try:
+            response = await client.get_object(Bucket=bucket_name, Key=band_path)
+            fileBand = await response['Body'].read()
+            with rasterio.io.MemoryFile(fileBand) as memfile:
+                with memfile.open(driver="GTiff", sharing=False) as band_file:
+                    band_data = band_file.read(1)  # open the tif image as a numpy array
+                    # Resize depending on the resolution
+                    if band_name in BANDS_20M and bNeedToResize:
+                        # Carry out a bicubic interpolation (TUB does exactly this)
+                        band_data = cv2.resize(band_data, dsize=(120, 120), interpolation=cv2.INTER_CUBIC)
+                    # We have already ignored the 60M ones, and we keep the 10M ones intact
+
+                    return band_data
+        except NoCredentialsError:
+            app.logger.warning("No AWS credentials found")
+            return None
+    async with asyncio.TaskGroup() as tg:
+        try:
+            for band_name in ALL_BANDS:
+                tasks.append(tg.create_task(fetch_band_data(band_name)))
+        except Exception as e:
+            app.logger.warning('exception'+str(e))
+    for task in tasks:
+        band_data = await task
+        bands_data.append(band_data)
+
+    bands_data = np.stack(bands_data)
+    bands_data = normalize_bands(bands_data)
+    return bands_data
+
+def read_data_s3(s3_path,keyString,NeedResize):
+    try:
+        cp = CloudPath("s3://"+s3_path+'/'+keyString)
+        ALL_BANDS = BANDS_10M + BANDS_20M
+        data = []
+        name = cp.name
+        bands_data = []
+        for band_name in ALL_BANDS:
+            band_path = cp.joinpath(f"{name}_{band_name}.tif")
+            with band_path.open('rb') as fileBand, rasterio.io.MemoryFile(fileBand) as memfile:
+                with memfile.open(driver="GTiff",sharing=False) as band_file:
+                    band_data = band_file.read(1)  # open the tif image as a numpy array
+                    # Resize depending on the resolution
+                    if band_name in BANDS_20M and NeedResize:
+                        # Carry out a bicubic interpolation (TUB does exactly this)
+                        band_data = cv2.resize(band_data, dsize=(120, 120), interpolation=cv2.INTER_CUBIC)
+                    # We have already ignored the 60M ones, and we keep the 10M ones intact
+
+                    bands_data.append(band_data)
+                band_file.close()
+
+        bands_data = np.stack(bands_data)
+        bands_data = normalize_bands(bands_data)
+    except Exception as e:
+        logging.info('exception in read_data_local '+str(e))
+        pass
+    return bands_data
+
+
+def read_data(CloudPath,folder,bNeedToResize) -> np.ndarray:
+    ALL_BANDS = BANDS_10M + BANDS_20M
+    data = []
+    name = folder
+    bands_data = []
+    metadata = {}
+    for band_name in ALL_BANDS:
+        band_path = CloudPath.joinpath(f"{name}_{band_name}.tif")
+        with band_path.open('rb') as fileBand, rasterio.io.MemoryFile(fileBand) as memfile:
+            with memfile.open(driver="GTiff",sharing=False) as band_file:
+                metadata[band_name] = band_file.meta
+                band_data = band_file.read(1)  # open the tif image as a numpy array
+                # Resize depending on the resolution
+                if band_name in BANDS_20M and bNeedToResize:
+                    # Carry out a bicubic interpolation (TUB does exactly this)
+                    band_data = cv2.resize(band_data, dsize=(120, 120), interpolation=cv2.INTER_CUBIC)
+                # We have already ignored the 60M ones, and we keep the 10M ones intact
+
+                bands_data.append(band_data)
+            band_file.close()
+
+    bands_data = np.stack(bands_data)
+    bands_data = normalize_bands(bands_data)
+    return bands_data,metadata
+
+def read_data_tar(tar,path,bNeedToResize) -> np.ndarray:
+    ALL_BANDS = BANDS_10M + BANDS_20M
+    bands_data = []
+    metadata = {}
+    for band_name in ALL_BANDS:
+        band_path = tar.extractfile(f"{path}_{band_name}.tif")
+        with rasterio.io.MemoryFile(band_path) as memfile:
+            with memfile.open(driver="GTiff",sharing=False) as band_file:
+                metadata[band_name] = band_file.meta
+                band_data = band_file.read(1)  # open the tif image as a numpy array
+                # Resize depending on the resolution
+                if band_name in BANDS_20M and bNeedToResize:
+                    # Carry out a bicubic interpolation (TUB does exactly this)
+                    band_data = cv2.resize(band_data, dsize=(120, 120), interpolation=cv2.INTER_CUBIC)
+                # We have already ignored the 60M ones, and we keep the 10M ones intact
+
+                bands_data.append(band_data)
+            band_file.close()
+
+    bands_data = np.stack(bands_data)
+    bands_data = normalize_bands(bands_data)
+    return bands_data,metadata
+        
+        
+def normalize_bands(
+        bands_data: np.ndarray,
+        desired_max: int = 1,
+        band_min: int = 0,
+        band_max: int = 10000,
+        normalize_per_band: bool = True,
+    ) -> np.ndarray:
+        bands_data = bands_data.astype(float)
+        if normalize_per_band:
+            for band_idx in range(bands_data.shape[0]):
+                np.clip(bands_data[band_idx], band_min, band_max)
+                bands_data[band_idx] = (
+                    bands_data[band_idx] / bands_data[band_idx].max()
+                ) * desired_max
+
+        else:
+            np.clip(bands_data, band_min, band_max)
+            bands_data = (bands_data / bands_data.max()) * desired_max
+        return bands_data
---
+++ dagger/src/bigearthnet/main.py
@@ -0,0 +1,312 @@
+import dagger
+from typing import Annotated
+from dagger import Doc, dag, function, object_type
+
+@object_type
+class Bigearthnet:
+    @function
+    def registry(
+        self,
+        bucket: Annotated[str, Doc("S3 Bucket")],
+        endpoint: Annotated[str, Doc("S3 Endpoint")],
+        accesskey: Annotated[str, Doc("S3 Access Key")],
+        secretkey: Annotated[str, Doc("S3 Secret Key")]
+    ) -> dagger.Service:
+        """Start and return a registry service."""
+        return (
+            dag.container()
+            .from_("registry:2.8.2")
+            .with_env_variable("REGISTRY_HTTP_ADDR", "0.0.0.0:80")
+            .with_env_variable("REGISTRY_HTTP_SECRET", secretkey)
+            .with_env_variable("REGISTRY_STORAGE", "s3")
+            .with_env_variable("REGISTRY_STORAGE_S3_REGION", "default")
+            .with_env_variable("REGISTRY_STORAGE_S3_BUCKET", bucket)
+            .with_env_variable("REGISTRY_STORAGE_S3_REGIONENDPOINT", endpoint)
+            .with_env_variable("REGISTRY_STORAGE_S3_ACCESSKEY", accesskey)
+            .with_env_variable("REGISTRY_STORAGE_S3_SECRETKEY", secretkey)
+            .with_exposed_port(80)
+            .as_service()
+        )
+
+    @function
+    async def build(
+        self,
+        bucket: Annotated[str, Doc("S3 Bucket")],
+        endpoint: Annotated[str, Doc("S3 Endpoint")],
+        access: Annotated[dagger.Secret, Doc("S3 Access Key")],
+        secret: Annotated[dagger.Secret, Doc("S3 Secret Key")],
+        repo: Annotated[str, Doc("Registry repo")],
+        tag: Annotated[str, Doc("Image tag")],
+        wkd: Annotated[
+            dagger.Directory,
+            Doc("Location of directory containing Dagger files"),
+        ],
+    ) -> str:
+        """Build and publish image from existing Dockerfile"""
+        accesskey = await access.plaintext()
+        secretkey = await secret.plaintext()
+        return await (
+            dag.container()
+            .from_("gcr.io/kaniko-project/executor:debug")
+            .with_service_binding(
+                "registry.local",
+                self.registry(bucket, endpoint, accesskey, secretkey)
+            )            
+            .with_mounted_directory("/workspace", wkd)
+            .with_exec(
+                [
+                    "/kaniko/executor",
+                    "--context", 
+                    "dir:///workspace/",
+                    "--dockerfile",
+                    "/workspace/Dockerfile",
+                    "--insecure",
+                    "--destination",
+                    f"registry.local/{repo}:{tag}"
+                ]
+            )
+            .stdout()
+        )
+    
+    @function
+    async def scan(
+        self,
+        bucket: Annotated[str, Doc("S3 Bucket")],
+        endpoint: Annotated[str, Doc("S3 Endpoint")],
+        access: Annotated[dagger.Secret, Doc("S3 Access Key")],
+        secret: Annotated[dagger.Secret, Doc("S3 Secret Key")],
+        severity: Annotated[str, Doc("Severity level")],
+        exit: Annotated[str, Doc("Exit code")],
+        repo: Annotated[str, Doc("Registry repo")],
+        tag: Annotated[str, Doc("Image tag")],
+        wkd: Annotated[
+            dagger.Directory,
+            Doc("Location of directory containing Dagger files"),
+        ],
+    ) -> dagger.File:
+        """Scan image to detect vulnerabilities"""
+        accesskey = await access.plaintext()
+        secretkey = await secret.plaintext()
+        template = (
+            dag.container()
+            .from_("alpine:latest")
+            .with_directory("/src", wkd)
+            .with_workdir("/src")
+            .with_exec(
+                [
+                    "wget",
+                    "https://raw.githubusercontent.com/aquasecurity/trivy/refs/heads/main/contrib/html.tpl"
+                ]
+            )
+        )
+        return await (
+            dag.trivy().base()
+            .with_service_binding(
+                "registry.local",
+                self.registry(bucket, endpoint, accesskey, secretkey)
+            )
+            .with_directory("/src", wkd)
+            .with_file("/src/html.tpl", template.file("/src/html.tpl"))
+            .with_exec(
+                [
+                    "trivy",
+                    "image",
+                    "--db-repository",
+                    "public.ecr.aws/aquasecurity/trivy-db",
+                    "--java-db-repository",
+                    "public.ecr.aws/aquasecurity/trivy-java-db",
+                    "--exit-code",
+                    exit,
+                    "--severity",
+                    severity,
+                    "--format",
+                    "template",
+                    "--template",
+                    "@/src/html.tpl",
+                    "--output",
+                    f"/src/vulnerabilities.html",
+                    "--insecure",
+                    f"registry.local/{repo}:{tag}"
+                ]
+            )
+            .file(f"/src/vulnerabilities.html")
+        )
+
+    @function
+    async def sbom(
+        self,
+        tag: Annotated[str, Doc("Image tag")],
+        wkd: Annotated[
+            dagger.Directory,
+            Doc("Location of directory containing Dagger files"),
+        ],
+    ) -> dagger.File:
+        """Scan image and produce SBOM file"""
+        return await (
+            dag.trivy().base()
+            .with_directory("/src", wkd)
+            .with_exec(
+                [
+                    "trivy",
+                    "image",
+                    "--db-repository",
+                    "public.ecr.aws/aquasecurity/trivy-db",
+                    "--java-db-repository",
+                    "public.ecr.aws/aquasecurity/trivy-java-db",
+                    "--exit-code",
+                    "0",
+                    "--format",
+                    "cyclonedx",
+                    "--output",
+                    f"/src/sbom-report.cdx.json",
+                    "--input",
+                    f"/src/{tag}"
+                ]
+            )
+            .file(f"/src/sbom-report.cdx.json")
+        )
+
+    @function
+    async def encode(
+        self,
+        username: Annotated[str, Doc("Registry username")],
+        password: Annotated[dagger.Secret, Doc("Registry password")],
+    ) -> str:
+        token = await password.plaintext()
+        return await (
+            dag.container()
+            .from_("alpine:latest")
+            .with_exec(
+                [
+                    "/bin/sh",
+                    "-c",
+                    f"printf {username}:{token} | base64"
+                ]
+            )
+            .stdout()
+        )
+
+    @function
+    async def push(
+        self,
+        bucket: Annotated[str, Doc("S3 Bucket")],
+        endpoint: Annotated[str, Doc("S3 Endpoint")],
+        access: Annotated[dagger.Secret, Doc("S3 Access Key")],
+        secret: Annotated[dagger.Secret, Doc("S3 Secret Key")],
+        registry: Annotated[str, Doc("Registry address")],
+        namespace: Annotated[str, Doc("Registry namespace")],
+        repo: Annotated[str, Doc("Registry repo")],
+        app: Annotated[str, Doc("Application name")],
+        srctag: Annotated[str, Doc("Source image tag")],
+        dsttag: Annotated[str, Doc("Destination image tag")],
+        username: Annotated[str, Doc("Registry username")],
+        password: Annotated[dagger.Secret, Doc("Registry password")],
+        wkd: Annotated[
+            dagger.Directory,
+            Doc("Location of directory containing Dagger files"),
+        ],
+    ) -> str:
+        """Build and publish image from existing Dockerfile"""
+        accesskey = await access.plaintext()
+        secretkey = await secret.plaintext()
+        blob = await self.encode(username, password)
+        config = (
+            dag.container()
+            .from_("alpine:latest")
+            .with_env_variable("CI_REGISTRY", registry)
+            .with_env_variable("CI_BLOB", blob)
+            .with_exec(
+                [
+                    "/bin/sh",
+                    "-c",
+                    "echo '{\"auths\":{\"'$CI_REGISTRY'\":{\"auth\":\"'$CI_BLOB'\"}}}' | sed 's/ //g'"
+                ],
+                redirect_stdout="/tmp/config.json"
+            )
+        )
+        return await (
+            dag.container(platform=dagger.Platform("linux/amd64"))
+            .with_service_binding(
+                "registry.local",
+                self.registry(bucket, endpoint, accesskey, secretkey)
+            )
+            .from_("rapidfort/skopeo-ib:v1.16.1")
+            .with_file("/tmp/config.json", config.file("/tmp/config.json"), owner="1000:1000")
+            .with_exec(
+                [
+                    "skopeo",
+                    "copy",
+                    "--src-tls-verify=false",
+                    "--src-no-creds",
+                    "--dest-authfile",
+                    "/tmp/config.json",
+                    f"docker://registry.local/{repo}:{srctag}",
+                    f"docker://{registry}/{namespace}/{repo}/{app}:{dsttag}"
+                ]
+            )
+            .stdout()
+        )
+    
+    @function
+    async def update(
+        self,
+        gitlab: Annotated[str, Doc("Gitlab address")],
+        repo: Annotated[str, Doc("Repository name")],
+        branch: Annotated[str, Doc("Service branch")],
+        username: Annotated[str, Doc("Repository username")],
+        password: Annotated[dagger.Secret, Doc("Repository password")],
+        wkd: Annotated[
+            dagger.Directory,
+            Doc("Location of directory containing Dagger files"),
+        ],
+    ) -> dagger.Directory:
+        """Create helm chart from cookiecutter template"""
+        token = await password.plaintext()
+        return await (
+            dag.container()
+            .from_("harness/cookiecutter:latest")
+            .with_directory("/src", wkd)
+            .with_workdir("/src")
+            .with_exec(
+                [
+                    "cookiecutter",
+                    "--no-input",
+                    "--config-file",
+                    "cookiecutter-config.yaml",
+                    "--checkout",
+                    f"{branch}",
+                    f"https://{username}:{token}@{gitlab}/eo4eu/eo4eu-cicd/cicd-infra/cookiecutter-helm-template.git"
+                ]
+            )
+            .directory(f"/src/{repo}")
+        )
+
+    @function
+    async def clean(
+        self,
+        bucket: Annotated[str, Doc("S3 Bucket")],
+        endpoint: Annotated[str, Doc("S3 Endpoint")],
+        access: Annotated[dagger.Secret, Doc("S3 Access Key")],
+        secret: Annotated[dagger.Secret, Doc("S3 Secret Key")],
+    ) -> str:
+        """Clean local registry."""
+        accesskey = await access.plaintext()
+        secretkey = await secret.plaintext()
+        return await (
+            dag.container()
+            .from_("amazon/aws-cli")
+            .with_env_variable("AWS_ACCESS_KEY_ID", accesskey)
+            .with_env_variable("AWS_SECRET_ACCESS_KEY", secretkey)
+            .with_exec(
+                [
+                    "aws",
+                    "--endpoint-url",
+                    f"{endpoint}",
+                    "s3",
+                    "rm",
+                    f"s3://{bucket}/docker",
+                    "--recursive"
+                ]
+            )
+            .stdout()
+        )
